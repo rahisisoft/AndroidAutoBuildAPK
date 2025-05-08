6107300ae7c4e00f5d8787f22b99e8860346497a
rahisisoft <rahisisoft@gmail.com>
2025-05-08 12:47:58 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/androidautobuildapk/MainActivity.kt
+++ app/src/main/java/com/example/androidautobuildapk/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.androidautobuildapk
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val webView = WebView(this)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://192.168.1.10/rahisisoft") // 🔁 Replace with your actual URL
+
+        setContentView(webView)
     }
 }
